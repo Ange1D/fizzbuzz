789e9597f60bddb0a0938c59af003c3c04d01229
Ange1D <47231898+Ange1D@users.noreply.github.com>
2022-05-03 21:56:20 -0500

    create data/explorers_test_file_1.json

--- test/utils/Reader.test.js
+++ test/utils/Reader.test.js
@@ -3,7 +3,7 @@ const Reader = require("../../lib/utils/Reader");
 describe("Unit Tests for Reader Class", () => {
     test('1) Read explorers.json', () => {
 
-      const explorers = Reader.readJsonFile("explorers.json");
+      const explorers = Reader.readJsonFile("test/data/explorers_test_file_1.json");
 
       expect(explorers).not.toBeUndefined()
       
