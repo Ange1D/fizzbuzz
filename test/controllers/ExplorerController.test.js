047b9c69aa88f92bb4839db840bbbfe52d15324f
Ange1D <47231898+Ange1D@users.noreply.github.com>
2022-05-07 15:46:52 -0500

    Update ExplorerController.test.js

---
+++ test/controllers/ExplorerController.test.js
@@ -0,0 +1,28 @@
+const ExplorerController = require("../../lib/controllers/ExplorerController");
+
+
+describe("Unit Tests Explorer Controller ", () => {
+    test("1) Filter Explorers By Mission", () => {
+
+        const explorersInNode = ExplorerController.getExplorersByMission("node");
+
+        expect(explorersInNode.length).toBe(10);
+      
+
+    });
+
+    test("2) Amount Of Explorers By Mission", () => {
+
+        const numberOfExplorerInNode = ExplorerController.getExplorersAmountByMission("node");
+
+        expect(numberOfExplorerInNode).toBe(10);
+    });
+
+    test("3) Explorers Usernames By Mission ", () => {
+
+        const explorersUsername = ExplorerController.getExplorersUsernamesByMission("node");
+
+        expect(explorersUsername).not.toBe("");
+    });
+
+});
