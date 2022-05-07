ceea75fcb0fc6cf61ee06735a0d132fc50139237
Ange1D <47231898+Ange1D@users.noreply.github.com>
2022-05-07 13:51:29 -0500

    Test getAmountOfExplorersByMission

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -1,9 +1,9 @@
 const ExplorerService = require("../../lib/services/ExplorerService");
 
+const explorers = [{mission:"node"}]
+
 describe("Unit Tests for ExplorerService Class", () => {
     test('1) Filter Explorers By Mission', () => {
-
-      const explorers = [{mission:"node"}]
 
       const explorersInNode = ExplorerService.filterByMision(explorers,"node");
 
@@ -11,4 +11,12 @@ describe("Unit Tests for ExplorerService Class", () => {
       
 
     });
+
+    test('2) Amount Of Explorers By Mission', () => {
+
+      const numberOfExplorerInNode = ExplorerService.getAmountOfExplorersByMission(explorers, "node");
+
+      expect(numberOfExplorerInNode).toBe(1);
+    });
+
   })
